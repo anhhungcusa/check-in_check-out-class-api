abb9d1625cba027c6d0a83c0de43e6c175f362f4
tampham97 <phamthanhtam10697@gmail.com>
2020-06-18 11:28:13 +0700

    test

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express()
 const port = 3000
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World test!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
