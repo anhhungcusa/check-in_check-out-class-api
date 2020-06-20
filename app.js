4dc4f62c4b7ec51042f7b01453c4cccb72693f72
@KhacHai <lekhachai9999@gmail.com>
2020-06-21 00:23:59 +0700

    setup environment

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
+const bodyParse = require("body-parser");
 const port = 3000
 
-const {env} = require('./config/globals') 
+// const {env} = require('./config/globals') 
 const { createConnection } = require("./db");
+
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParse.json());
+
+
+const initRestRoutes = require("./routes/index");
+
+// error handler middleware
+const errorHandler = require("./middlewares/error-handler");
 
 // connect mongoose
 createConnection();
+
+
+// init routes
+initRestRoutes(app);
+app.use(errorHandler);
 
 app.get('/', (req, res) => res.send('Hello World test!'))
 
--- config/globals.js
+++ config/globals.js
@@ -1,7 +1,8 @@
 module.exports = {
 	env: {
 		PORT: process.env.PORT || 4000,
-		MONGODB_CONNECT_URI: process.env.NODE_ENV === 'production' ? process.env.MONGODB_CONNECT_URI : 'mongodb://localhost:27017/instagram?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+		// MONGODB_CONNECT_URI: process.env.NODE_ENV === 'production' ? process.env.MONGODB_CONNECT_URI : 'mongodb://localhost:27017/QRCode?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false',
+		MONGODB_CONNECT_URI: process.env.MONGODB_CONNECT_URI,
 		NODE_ENV: process.env.NODE_ENV || 'development',
 		JWT_SECRET_KEY: process.env.JWT_SECRET_KEY|| 'test'
 	},
