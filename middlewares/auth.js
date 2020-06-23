af40e2a9fe93f0b93e9ac26e34d8a19812c3b196
@KhacHai <lekhachai9999@gmail.com>
2020-06-23 19:43:49 +0700

    add auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+const { Exception } = require("../utils");
+const { verifyToken } = require("../utils/jwt");
+const { statusCodes } = require("../config/globals");
+module.exports.isAuthorized = async (req, res, next) => {
+  let token = req.get("authorization");
+  try {
+    if (!token) throw new Exception("Token is not valid");
+    if (token.indexOf("Bearer ") !== 0)
+      throw new Exception("Token is not Bearer Token");
+    token = token.slice(7, token.length).trimLeft();
+    const decoded = await verifyToken(token, process.env.JWT_SECRET_KEY);
+    req.auth = decoded;
+    next();
+  } catch ({ message = "Token is not valid" }) {
+    res.status(statusCodes.UNAUTHORIZED).send({ message });
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 //routes
-const session = require('./sesion.route');
-const room = require('./room.route');
-const user = require('./user.route');
-const auth = require('./auth.route');
+const session = require("./sesion.route");
+const room = require("./room.route");
+const user = require("./user.route");
+const auth = require("./auth.route");
 
-
+// middlewares
+const authMiddleware = require("../middlewares/auth");
 
 // init rest routes
-module.exports = router => {
-    const prefix = "/api/v1"
-    router.use(`${prefix}/sessions`, session)
-    router.use(`${prefix}/rooms`, room)
-    router.use(`${prefix}/users`, user)
-    router.use(`${prefix}/auth`, auth)
-}
+module.exports = (router) => {
+  const prefix = "/api/v1";
+  router.use(`${prefix}/sessions`, authMiddleware.isAuthorized, session);
+  router.use(`${prefix}/rooms`, authMiddleware.isAuthorized, room);
+  router.use(`${prefix}/users`, authMiddleware.isAuthorized, user);
+  router.use(`${prefix}/auth`, authMiddleware.isAuthorized, auth);
+};
