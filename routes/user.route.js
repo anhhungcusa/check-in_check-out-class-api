abe8f384c59115b21571b73662cc8d910b5417fc
@KhacHai <lekhachai9999@gmail.com>
2020-06-23 18:38:31 +0700

    feat(QP-37): design api get users

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const userController = require('../controllers/user.controller')
 
 router.get('', userController.getAll)
+
 router.post('', userController.register)    
 
 module.exports = router
