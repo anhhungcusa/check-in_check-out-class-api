762c08d8a86f7a6e0d6282311f4aae7a3eca3c15
anhhungcusa <nguyenphucnguyenvy@gmail.com>
2020-06-27 01:54:06 +0700

    feat(QP-48): add populate for models

--- models/qr_session.model.js
+++ models/qr_session.model.js
@@ -2,15 +2,11 @@ const {Schema, model} = require('mongoose')
 const {ObjectId} = Schema.Types
 
 const qrSessionSchema = new Schema({
-    sessionId: {
-        type: ObjectId,
-        required: true,
-        index: true
-    },
     userId: {
         type: ObjectId,
         required: true,
-        index:true
+        index:true,
+        unique: true
     }
 })
 
